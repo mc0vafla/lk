fd9a79c520ae0ded2836be23ceeff024feae5b1e
mc0vafla <sssskrol@gmail.com>
2026-08-09 09:19:43 +0200

    1

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 zoxide init fish | source
 set -gx EDITOR nvim
+export LIBVA_DRIVER_NAME=nvidia
+export XDG_SESSION_TYPE=wayland
+export GBM_BACKEND=nvidia-drm
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_NO_HARDWARE_CURSORS=1
 export WLR_RENDERER=vulkan
 export SDL_VIDEODRIVER=wayland
